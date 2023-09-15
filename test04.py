892f356bed83fc8acf63fc236050504f55f80e0c
chanakan6519410016 <s65194100116@sau.ac.th>
2023-09-15 12:34:58 +0700

    v.2

--- test04.py
+++ test04.py
@@ -14,3 +14,31 @@
 # sam of 5 number is : <output>
 # Average of 5 number is : <output> (ขอทศนิยม 4 ตำำแหน่ง)
 #===================================
+def inputnumber() :
+    n1=int(input('ใส่เลขที่ 1 :'))
+    n2=int(input('ใส่เลขที่ 2 :'))
+    n3=int(input('ใส่เลขที่ 3 :'))
+    n4=int(input('ใส่เลขที่ 4 :'))
+    n5=int(input('ใส่เลขที่ 5 :'))
+    return n1, n2, n3, n4, n5
+
+def Sumofnumber(n1, n2, n3, n4, n5):
+    Sumofnumber = (n1+ n2+ n3+ n4+ n5)
+    return Sumofnumber
+
+def Averageofnumber(n1, n2, n3, n4, n5):
+    Averageofnumber = ((n1+n2+n3+n4+n5)/5)
+    return Averageofnumber
+
+def show(n1,n2,n3,n4,n5,Sumofnumber,Averageofnumber):
+    print(f'ผลรวมของเลขทั้ง 5 :{Sumofnumber} ')
+    print(f'ค่าเฉลี่ยของเลขทั้ง 5 : {Averageofnumber:.4f} ')
+    
+print("=========================================")
+print("======== Program Average 5 Number========")
+print("=========================================")
+n1,n2,n3,n4,n5 = inputnumber()
+print("=========================================")
+Sumofnumber = Sumofnumber(n1,n2,n3,n4,n5)
+Averageofnumber = Averageofnumber(n1,n2,n3,n4,n5)
+show(n1,n2,n3,n4,n5,Sumofnumber,Averageofnumber)
